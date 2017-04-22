79e50f7b6b70dc34b3f5513583510d3d17ba12f3
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-04-22 03:11:45 +0000

    fix password required when update

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,8 +1,12 @@
 module WikisHelper
   def getUser(id)
-    #if an author of a wiki is deleted
+    #if an author of a wiki is deleted, or no name has been edited
     begin
-      User.find(id).email
+      if User.find(id).name
+        User.find(id).name + ": " + User.find(id).email
+      else
+        User.find(id).email
+      end
     rescue ActiveRecord::RecordNotFound  
       return "an anonymous user"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   # validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 #   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: false
+  validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
