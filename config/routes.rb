27a36900ca71a73a1369061fc474c7eb00ad23bc
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-04-25 17:55:24 +0000

    seed admin, users, and wikis

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   
   devise_for :users
+  resources :users
   
   get 'welcome/index'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(
+  name: 'Admin User',
+  password: '123qwe',
+  email: 'admin@user.com',
+  role: 'admin')
+  
+5.times do
+  fake_first_name = Faker::Name.first_name
+  fake_last_name = Faker::Name.last_name
+  User.create!(
+    name: fake_first_name + " " + fake_last_name,
+    password: 'password',
+    email: Faker::Internet.email(fake_first_name + "." + fake_last_name)
+  )
+end
+
+5.times do
+  fake_title = Faker::Lorem.word
+  Wiki.create!(
+    title: fake_title,
+    body: fake_title + ": " + Faker::Lorem.paragraph,
+    user: User.all.sample
+  )
+ end
+
+puts "Seed finished"
+puts "#{User.count} fake users created"
+puts "#{Wiki.count} fake wikis created"
